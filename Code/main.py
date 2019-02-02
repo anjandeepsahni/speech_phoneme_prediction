4566f306933db6a8983934eec3baacb569785f99
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2019-02-02 01:18:35 -0500

    Added support for testing and generating the result csv file. Works fine.

--- Code/main.py
+++ Code/main.py
@@ -8,6 +8,20 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import time
 import tqdm as tqdm
+import csv
+
+MODEL_PATH = './../Models'
+TEST_RESULT_PATH = './../Results'
+
+def save_test_results(predictions):
+    predictions = list(predictions.numpy())
+    predictions_count = list(range(len(predictions)))
+    csv_output = [[i,j] for i,j in zip(predictions_count,predictions)]
+    result_file_path = os.path.join(TEST_RESULT_PATH, 'result_{}.csv'.format(time.strftime("%Y%m%d-%H%M%S")))
+    with open(result_file_path, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['id', 'label'])
+        csv_writer.writerows(csv_output)
 
 def train_model(model, train_loader, criterion, optimizer, device):
     model.train()
@@ -48,7 +62,7 @@ def val_model(model, val_loader, criterion, device):
             correct_predictions += (predicted == target).sum().item()
             loss = criterion(outputs, target).detach()
             running_loss += loss.item()
-            print('Val Iteration: %d/%d Loss = %5.4f' % \
+            print('Validation Iteration: %d/%d Loss = %5.4f' % \
                     (batch_idx+1, len(val_loader), (running_loss/(batch_idx+1))), \
                     end="\r", flush=True)
         end_time = time.time()
@@ -58,16 +72,38 @@ def val_model(model, val_loader, criterion, device):
                 (running_loss, acc, end_time - start_time))
         return running_loss, acc
 
+def test_model(model, test_loader, device):
+    with torch.no_grad():
+        model.eval()
+        model.to(device)
+        start_time = time.time()
+        all_predictions = []
+        for batch_idx, data in enumerate(test_loader):
+            data = data.to(device)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            all_predictions.append(predicted)
+            print('Test Iteration: %d/%d' % (batch_idx+1, len(test_loader)), end="\r", flush=True)
+        # Join list of predicted tensors.
+        all_predictions = torch.cat(all_predictions, 0)
+        # Save predictions in csv file.
+        save_test_results(all_predictions)
+        end_time = time.time()
+        print('\nTotal Test Predictions: %d Time: %d s' % (all_predictions.size()[0], end_time - start_time))
+
 if __name__ == "__main__":
+    reload_model = True
+    testing_only = True
+
     # Instantiate speech dataset.
     speechTrainDataset = SpeechDataset(mode='train')
     speechTestDataset = SpeechDataset(mode='test')
     speechValDataset = SpeechDataset(mode='dev')
-    train_loader = DataLoader(speechTrainDataset, batch_size=400,
+    train_loader = DataLoader(speechTrainDataset, batch_size=500,
                                 shuffle=False, num_workers=4)
-    test_loader = DataLoader(speechTestDataset, batch_size=4,
+    test_loader = DataLoader(speechTestDataset, batch_size=500,
                                 shuffle=False, num_workers=4)
-    val_loader = DataLoader(speechValDataset, batch_size=400,
+    val_loader = DataLoader(speechValDataset, batch_size=500,
                             shuffle=False, num_workers=4)
 
     model = SpeechClassifier()
@@ -78,19 +114,36 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters())
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    if reload_model:
+        # Find the list of all saved models.
+        model_list = [f for f in os.listdir(MODEL_PATH) if os.path.isfile(os.path.join(MODEL_PATH, f))]
+        if model_list:
+            # Load the last saved model.
+            model_path = os.path.join(MODEL_PATH, model_list[-1])
+            model.load_state_dict(torch.load(model_path))
+
     n_epochs = 1
     Train_loss = []
     Val_loss = []
     Val_acc = []
-    #Test_loss = []
-    #Test_acc = []
 
     print('='*20)
-    for i in range(n_epochs):
-        print('Epoch: %d/%d' % (i+1,n_epochs))
-        train_loss = train_model(model, train_loader, criterion, optimizer, device)
-        val_loss, val_acc = val_model(model, val_loader, criterion, device)
-        Train_loss.append(train_loss)
-        Val_loss.append(val_loss)
-        Val_acc.append(val_acc)
+
+    if not testing_only:
+        for i in range(n_epochs):
+            print('Epoch: %d/%d' % (i+1,n_epochs))
+            train_loss = train_model(model, train_loader, criterion, optimizer, device)
+            val_loss, val_acc = val_model(model, val_loader, criterion, device)
+            Train_loss.append(train_loss)
+            Val_loss.append(val_loss)
+            Val_acc.append(val_acc)
+            print('='*20)
+
+        # Save model parameters.
+        finalValAcc = '%.3f'%(Val_acc[-1])
+        model_path = os.path.join(MODEL_PATH, 'model_{}_val_{}.pt'.format(time.strftime("%Y%m%d-%H%M%S"), finalValAcc))
+        torch.save(model.state_dict(), model_path)
+    else:
+        # Only testing the model.
+        test_model(model, test_loader, device)
         print('='*20)
